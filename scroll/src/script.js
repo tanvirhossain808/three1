9a7b1966610e8e4b6b44e7b802bb10af8dc7ce20
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-02-27 12:47:57 +0600

    adding the scroll

--- scroll/src/script.js
+++ scroll/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from "gsap"
 
 /**
  * Debug
@@ -14,6 +15,7 @@ const parameters = {
 gui
     .addColor(parameters, 'materialColor').onChange(() => {
         material.color.set(parameters.materialColor)
+        particlesMateris.color.set(parameters.materialColor)
     })
 
 /**
@@ -54,13 +56,35 @@ mesh1.position.y = -objectDistance * 0
 mesh2.position.y = -objectDistance * 1
 mesh3.position.y = -objectDistance * 2
 // mesh3.position.y = -objectDistance * 2
-
 const sectionMeshes = [mesh1, mesh2, mesh3]
+
+//paricles
+//geomtery
+const particarCount = 200
+const postionArray = new Float32Array(particarCount * 3)
+for (let i = 0; i < particarCount; i++) {
+    postionArray[i * 3 + 0] = (Math.random() - .5) * 10
+    postionArray[i * 3 + 1] = objectDistance * .5 - Math.random() * objectDistance * 3
+    postionArray[i * 3 + 2] = (Math.random() - .5) * 10
+}
+const particleGeometry = new THREE.BufferGeometry();
+particleGeometry.setAttribute("position", new THREE.BufferAttribute(postionArray, 3))
+const particlesMateris = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    size: .03,
+    sizeAttenuation: true
+})
+const particles = new THREE.Points(particleGeometry, particlesMateris)
+scene.add(particles)
+mesh1.position.x = 2
+mesh2.position.x = - 2
+mesh3.position.x = 2
 //light
 
 const directionalLight = new THREE.DirectionalLight("#ffffff", 1)
 directionalLight.position.set(1, 1, 0)
 scene.add(directionalLight)
+
 /**
  * Sizes
  */
@@ -78,6 +102,9 @@ window.addEventListener('resize', () => {
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
+    //group
+
+
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -86,10 +113,12 @@ window.addEventListener('resize', () => {
 /**
  * Camera
  */
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -106,24 +135,53 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 */
 //scrollY
 let scrollY = window.scrollY
+let currentSection = 0
 window.addEventListener("scroll", () => {
     scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+    if (newSection != currentSection) {
+
+        currentSection = newSection
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation, {
+            duration: 1.5,
+            ease: "power2.inOUt",
+            x: "+=6",
+            y: "+=3",
+            z: "+=1.5"
+
+        }
+        )
+    }
 })
 
+//Cursor
+const cursor = {};
+cursor.x = 0
+cursor.y = 0
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - .5
+    cursor.y = e.clientY / sizes.height - .5
+})
 
 const clock = new THREE.Clock()
-
+let previouseTime = 0
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previouseTime
+    previouseTime = elapsedTime
     //animate camera
-    const distance = -scrollY / sizes.height * objectDistance
-    console.log(distance);
-    camera.position.y = distance
-
+    camera.position.y = -scrollY / sizes.height * objectDistance
+    const parallaxX = cursor.x
+    const parallaxY = -cursor.y
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    // console.log(parallaxY - cameraGroup.position.y, 'heyu');
     //animate meshes
     for (const mesh of sectionMeshes) {
-        mesh.rotation.x = elapsedTime * .1
-        mesh.rotation.y = elapsedTime * .12
+        mesh.rotation.x += deltaTime * .1
+        mesh.rotation.y += deltaTime * .12
     }
 
     // Render
